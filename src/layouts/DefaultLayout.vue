a7f783b5de4d2dd20bb9f6a93f3cc055909269e6
WonTaeJang <jwt0212@gmail.com>
2025-05-10 11:37:38 +0900

    feat: snackbar 적용

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -5,14 +5,28 @@
       v-model="isFolded"
     />
 
-    <!-- page -->
-    <section class="page-container">
-      <!-- side bar -->
-      <DefaultSidebar
-        :is-folded="isFolded"
-      />
+    <Teleport to="#snackbar">
+      <SnackBar />
+    </Teleport>
 
-      <article class="main-container">
+    <!-- side bar -->
+    <DefaultSidebar
+      :is-folded="isFolded"
+    />
+
+    <!-- page -->
+    <section
+      :class="[
+        'page-container',
+        {
+          'page-container-folded': isFolded,
+          'page-container-open': !isFolded
+        }
+      ]"
+    >
+      <article 
+        :class="['main-container']"
+      >
         <router-view />
       </article>
     </section>
@@ -22,6 +36,7 @@
 <script setup>
 import DefaultHeader from '@ui/header/DefaultHeader.vue'
 import DefaultSidebar from '@ui/sidebar/DefaultSidebar.vue'
+import SnackBar from '@ui/snackbar/SnackBar.vue'
 import { ref } from 'vue'
 import { useCoreStore } from '@store/core'
 import { storeToRefs } from 'pinia'
@@ -37,19 +52,33 @@ const { isFolded } = storeToRefs(coreStore)
 @use '@style/common.scss' as common;
 
 .default-layout {
+  position: relative;
   background: #fff;
   width: 100%;
   height: 100vh;
   padding-top: variables.$header-height;
-  
 
   .page-container {
+    overflow-y: scroll;
     height: 100%;
-    display: flex;
+    width: 100%;
+
+    &-folded {
+      padding-left: variables.$nav-folded;
+    }
+
+    &-open {
+      padding-left: variables.$nav-wide;
+    }
   }
 
   .main-container {
-    flex: 1;
+    min-width: 600px;
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 20px;
+    box-sizing: border-box;
+    overflow-x: auto;
   }
 }
 </style>
